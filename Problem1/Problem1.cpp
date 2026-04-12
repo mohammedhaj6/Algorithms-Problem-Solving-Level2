40329e24d4a3398455ffd352c0f3baefa7ff3f61
ETO <mohammedalhajmohammed6@gmail.com>
2026-04-12 09:37:57 +0300

    add Problem1,2

--- Problem1/Problem1.cpp
+++ Problem1/Problem1.cpp
@@ -1,20 +1,43 @@
-// Problem1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<string>
+
+using namespace std;
+
+
+void buildHeader() {
+    cout << "\t\t\tMultiplication Table From 1 To 10\n\n";
+
+    cout << "\t";
+    for (int i = 1; i <= 10; i++) {
+        cout << i << "\t";
+    }
+
+    cout << endl;
+}
+
+void buildSeparator() {
+    cout << "-----------------------------------------------------------------------------------\n";
+}
+
+void buildMultiplicationTable() {
+
+    for (int i = 1; i <= 10; i++) {
+
+        cout <<" " << i << "\t| ";
+
+       for (int j = 1; j <= 10; j++) {
+            cout << i * j << "\t";
+        }
+
+        cout << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    buildHeader();
+    buildSeparator();
+    buildMultiplicationTable();
+	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Problem2/Problem2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include<string>
+
+using namespace std;
+
+
+enum enIsPrime
+{
+	prime = 1, NotPrime = 0
+};
+
+int readPositveNumber() {
+	int num = 0;
+	do
+	{
+		cout << "Enter A Positve Number  ?\n";
+		cin >> num;
+	} while (num <= 0);
+
+
+	return num;
+}
+
+enIsPrime cheakIfPrimeOrNot(int num) {
+	int m = sqrt(num);;
+
+	for (int counter = 2; counter <= m; counter++)
+	{
+		if (num % counter == 0)
+			return enIsPrime::NotPrime;
+	}
+	return enIsPrime::prime;
+}
+
+void printPrimeNumbersFrom1ToN(int num) {
+	for (int i = 2; i <= num; i++)
+	{
+		if (cheakIfPrimeOrNot(i) == enIsPrime::prime)
+			cout << endl << i << endl;
+	}
+}
+
+
+int main()
+{
+	printPrimeNumbersFrom1ToN(readPositveNumber());
+}
