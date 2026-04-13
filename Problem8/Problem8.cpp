6598690b6cb9081a007008a707c6bc4c0608a6c5
ETO <mohammedalhajmohammed6@gmail.com>
2026-04-13 19:49:37 +0300

    add problem8

---
+++ Problem8/Problem8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int readNumber() {
+	int num;
+	cout << "Enter Your Number ?\n";
+	cin >> num;
+	cout << endl;
+	return num;
+}
+
+int readDigit() {
+	int digit;
+	cout << "Enter Your Digit ?\n";
+	cin >> digit;
+	cout << endl;
+	return digit;	
+}
+
+void CountDigitFrequency(int digit, int num) {
+	int remainder,counter = 0;
+	
+	while (num > 0) {
+		
+		remainder = num % 10;
+		num = num / 10;
+		if (digit== remainder) {
+			counter++;
+		}
+		
+
+	}
+	cout << "Digit " << digit << " is " << counter << " Times(s)\n";
+}
+
+int main() {
+	CountDigitFrequency(readDigit(), readNumber());
+}
